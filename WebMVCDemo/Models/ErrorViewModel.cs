a3083a5ef1dab9e0729818931b3dbd51ee6244b2
Shonsu <shonsu82@gmail.com>
2024-04-09 10:42:20 +0200

    InitailCommit

---
+++ WebMVCDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebMVCDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
